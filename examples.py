Removed softmax for now, too confusing atm fixed demo

examples.py
@@ -139,21 +139,15 @@ def complete_a_picture(viz_client):
     :param viz_client: An instance of NumpynetVizClient
     """
     x_min = 0.0; x_max = 2.0; y_min = 0; y_max = 1.0
-    # x_min = 0.4; x_max = 0.6; y_min = 0.45; y_max = 0.55
-
     train_in, train_out = make_checkerboard_training_set(num_points=1000, noise=0.00, randomize=True,
                                                          x_min=x_min, x_max=x_max, y_min=y_min, y_max=y_max)
 
     # x_min = 0.0; x_max = 1.0; y_min = 0.0; y_max = 1.0
     # train_in, train_out = make_smiley_training_set(num_points=1000)
 
-    viz_client.plot_2d_classes(train_in,
-                               train_out,
-                               title="Training data",
-                               x_min=x_min,
-                               x_max=x_max,
-                               y_min=y_min,
-                               y_max=y_max,
+    viz_client.plot_2d_classes(train_in, train_out, title="Training data",
+                               x_min=x_min, x_max=x_max,
+                               y_min=y_min, y_max=y_max,
                                delta=0.01)
 
     training_size = train_in.shape[0]
@@ -163,7 +157,6 @@ def complete_a_picture(viz_client):
     numpy_net = NumpyNet(num_features, batch_size,
                          num_hidden=5, hidden_sizes=[8, 32, 64, 32, 8],
                          activation=["sigmoid", "tanh", "tanh", "tanh", "tanh", "sigmoid"],
-                         # activation="relu",
                          learning_rate=0.0002,
                          dropout_rate=None, weight_decay=None,
                          random_seed=None)
@@ -183,11 +176,6 @@ def plot_activations():
         x = np.linspace(-10.0, 10.0, 100)
         y = common.Activation(activation).function(x, deriv=False)
         dy = common.Activation(activation).function(x, deriv=True)
-        print("\n" + activation)
-        print("==================================")
-        print(x)
-        print(y)
-        print("\n")
         viz_client.plot_func(x, y, title=activation)
         viz_client.plot_func(x, dy, title="d_"+activation)
 
@@ -224,9 +212,9 @@ if __name__ == '__main__':
     # Set up NumpynetVizClient
     viz_client = NumpynetVizClient(viz=viz)
 
-    plot_activations()
+    # plot_activations()
 
-    # complete_a_picture(viz_client)
+    complete_a_picture(viz_client)
 
     # load_a_model("./numpynet_best_model.pickle")
 

numpynet/common.py
@@ -12,7 +12,7 @@ class Activation:
     A class to hold all of the activation functions, ensures all have derivatives
     """
     function = None
-    available = ["tanh", "_tanhpos", "bugged_sigmoid", "sigmoid", "relu", "softmax", "stablesoftmax"]
+    available = ["tanh", "_tanhpos", "sigmoid", "relu"]
 
     def __init__(self, choice="sigmoid"):
         """
@@ -25,16 +25,10 @@ class Activation:
             self.function = self._tanh
         elif choice == "_tanhpos":
             self.function = self._tanhpos
-        elif choice == "bugged_sigmoid":
-            self.function = self._bugged_sigmoid
         elif choice == "sigmoid":
             self.function = self._sigmoid
         elif choice == "relu":
             self.function = self._relu
-        elif choice == "softmax":
-            self.function = self._softmax
-        elif choice == "stablesoftmax":
-            self.function = self._stablesoftmax
 
     @staticmethod
     def _tanh(x, deriv=False):
@@ -55,16 +49,6 @@ class Activation:
         return (np.tanh(x) + 1.0) / 2.0
 
     @staticmethod
-    def _bugged_sigmoid(x, deriv=False):
-        """
-        A function meant to be sigmoid and a bug i made for derivative
-        TODO Why does this work? Going to leave it for now, because it's interesting
-        """
-        if deriv:
-            return x * (1.0 - x)  # This is wrong.
-        return 1.0 / (1.0 + np.exp(-x))
-
-    @staticmethod
     def _sigmoid(x, deriv=False):
         """
         The sigmoid function and its derivative
@@ -81,28 +65,7 @@ class Activation:
         """
         if deriv:
             return 1.0 * (x > 0)
-        return np.maximum(x, 0, x)
-
-    @staticmethod
-    # TODO: Check this again
-    def _softmax(x, deriv=False):
-        """
-        Compute softmax values for each sets of scores in x.
-        """
-        e_x = np.exp(x - np.max(x))
-        if deriv:
-            return (x - np.max(x)) * e_x / e_x.sum(axis=0)
-        return e_x / e_x.sum(axis=0)
-
-    @staticmethod
-    # TODO: this needs to be completed
-    def _stablesoftmax(x, deriv=False):
-        """
-        Compute the softmax of vector x in a numerically stable way.
-        """
-        shiftx = x - np.max(x)
-        exps = np.exp(shiftx)
-        return exps / np.sum(exps)
+        return np.maximum(x, 0)
 
 
 def predict_2d_space(net, delta=0.05):